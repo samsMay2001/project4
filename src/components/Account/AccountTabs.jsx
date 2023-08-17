1ee103a52ea8fbf53073c1de36a528a0f3a7c010
sam <samscloud2@gmail.com>
2023-08-17 09:07:20 -0300

    logout feature

--- src/components/Account/AccountTabs.jsx
+++ src/components/Account/AccountTabs.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../../App/appContext";
 
 function AccountTabs() {
+    const {setLoggedUser} = useAppContext()
+    function logOut() {
+        localStorage.clear()
+        setLoggedUser(null)
+    }
     return ( 
         <div className="col-lg-4">
             <div className="ltn__tab-menu-list mb-50">
@@ -12,7 +18,7 @@ function AccountTabs() {
                     <a data-bs-toggle="tab" href="#ltn_tab_1_5">My Properties <i className="fa-solid fa-list" /></a>
                     <a data-bs-toggle="tab" href="#ltn_tab_1_6">Favorited Properties <i className="fa-solid fa-heart" /></a>
                     <a data-bs-toggle="tab" href="#ltn_tab_1_7">Add Property <i className="fa-solid fa-map-location-dot" /></a>
-                    <Link to="/login">Logout <i className="fas fa-sign-out-alt" /></Link>
+                    <Link to="/login" onClick={logOut}>Logout <i className="fas fa-sign-out-alt" /></Link>
                 </div>
             </div>
         </div>
